d6e975f949aa4a17b0a82f1dd482819d62a836ae
teguhirvan85-hue <teguhirvan85@gmail.com>
2026-01-04 19:43:56 +0700

    Footer: init

--- src/comps/primitive/Button.tsx
+++ src/comps/primitive/Button.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Button({ children }: { children: ReactNode }) {
   return (
-    <button className="cursor-pointer capitalize font-medium bg-white py-3 px-10">
+    <button className="cursor-pointer capitalize font-medium font-inter bg-white py-3 px-10">
       {children}
     </button>
   );
--- src/comps/ui/Footer.tsx
+++ src/comps/ui/Footer.tsx
@@ -1,7 +1,64 @@
+import Link from "next/link";
+
+const SOCIAL_MEDIA = [
+  {
+    src: "./img/instagram.png",
+    href: "#",
+  },
+  {
+    src: "./img/facebook.png",
+    href: "#",
+  },
+  {
+    src: "./img/linkedin.png",
+    href: "#",
+  },
+  {
+    src: "./img/twitter.png",
+    href: "#",
+  },
+  {
+    src: "./img/youtube.png",
+    href: "#",
+  },
+];
+
 export default function Footer() {
+  const optClass = "px-3";
   return (
-    <footer>
-      <h2 className="text-4xl font-philosopher">footer</h2>
+    <footer className="px-10 py-6">
+      <div className="ml-auto w-1/2 max-w-150">
+        <p className="text-3xl capitalize text-[#6C737F] mb-10 leading-[140%]">
+          Artiface presents
+          <span className="text-black underline mx-[.5em] capitalize">
+            Curated objects, balancing historical context, considered exchange,
+            and long-term
+          </span>
+          Relevance across markets.
+        </p>
+        <ul className="flex item-center gap-4">
+          {SOCIAL_MEDIA.map((item, i) => {
+            return (
+              <Link key={i} href={item.href}>
+                <img className="w-6" src={item.src} alt="" />
+              </Link>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="flex items-end justify-between">
+        <Link href="#">
+          <img className="h-20.5" src="./img/logo-footer.png" alt="artifacte" />
+        </Link>
+
+        <p>©Artifacte. All Rights Reserved.</p>
+
+        <ul className="flex">
+          <li className={`${optClass}`}>Terms & Conditions</li>
+          <li className={`${optClass} border-x`}>Privacy Policy</li>
+          <li className={`${optClass}`}>Risk Disclosure</li>
+        </ul>
+      </div>
     </footer>
   );
 }
