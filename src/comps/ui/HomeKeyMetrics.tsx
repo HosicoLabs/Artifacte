715f1b94978922f2f07ac7e976222776915eaf6c
teguhirvan85-hue <teguhirvan85@gmail.com>
2026-01-05 11:46:59 +0700

    HomeKeyMetrices: init second section

--- src/comps/ui/HomeKeyMetrics.tsx
+++ src/comps/ui/HomeKeyMetrics.tsx
@@ -1,3 +1,8 @@
+"use client";
+import { useEffect, useState } from "react";
+import Button from "../primitive/Button";
+import { twMerge } from "tailwind-merge";
+
 function FirstSection() {
   const DETAILS = [
     {
@@ -63,9 +68,79 @@ function FirstSection() {
 }
 
 function SecondSection() {
+  const [selected, setSelected] = useState<string>("");
+  const STEPS = [
+    {
+      icon: "./img/battery-full.png",
+      title: "describe the asset",
+      desc: "Provide key details, ownership information, and provenance to represent your real-world asset as an NFT.",
+    },
+    {
+      icon: "./img/upload.png",
+      title: "upload proof & documentation",
+      desc: "Provide key details, ownership information, and provenance to represent your real-world asset as an NFT.",
+    },
+    {
+      icon: "./img/speaker.png",
+      title: "review and submit",
+      desc: "Provide key details, ownership information, and provenance to represent your real-world asset as an NFT.",
+    },
+    {
+      icon: "./img/feather.png",
+      title: "asset verification",
+      desc: "Provide key details, ownership information, and provenance to represent your real-world asset as an NFT.",
+    },
+    {
+      icon: "./img/bot.png",
+      title: "auction goes live",
+      desc: "Provide key details, ownership information, and provenance to represent your real-world asset as an NFT.",
+    },
+  ];
+
   return (
-    <section>
-      <h2>second section</h2>
+    <section className="flex justify-between px-10 py-20 items-start">
+      <div className="max-w-[31%]">
+        <h2 className="font-medium text-[40px] pb-6">
+          How to Submit Real-World Asset NFTs for Auction
+        </h2>
+        <p className="text-xl">
+          List your real-world assets as NFTs in just a few simple steps—
+          secure, transparent, and globally accessible.
+        </p>
+        <ul className="pt-10 pb-20">
+          {STEPS.map((item, i) => {
+            const defaultClass =
+              "p-5 border-l-2 border-l-[#cccccc] flex items-start gap-4 overflow-hidden max-h-[3em] transition duration-300 ease-in-out";
+            const selectedClass = "border-l-2 border-l-black max-h-full";
+            const finalClass = twMerge(
+              defaultClass,
+              selected == item.title ? selectedClass : ""
+            );
+            return (
+              <li
+                key={i}
+                onClick={() => setSelected(() => item.title)}
+                className={finalClass}
+              >
+                <img className="w-5" src={item.icon} alt="" />
+                <div>
+                  <p className="mb-3 font-medium text-xl capitalize">
+                    {item.title}
+                  </p>
+                  <p className="text-xl text-[#6C737F]">{item.desc}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+
+        <Button className="text-white bg-[#162737]">submit now</Button>
+      </div>
+      <img
+        className="basis-1/2 max-w-1/2"
+        src="./img/home-key-metrices-bot.png"
+        alt=""
+      />
     </section>
   );
 }
