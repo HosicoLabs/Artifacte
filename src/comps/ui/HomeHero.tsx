3f69bac2cfc7350e2958a4b6a43b22f7550050b3
teguhirvan85-hue <teguhirvan85@gmail.com>
2026-01-06 11:56:16 +0700

    HomeHero: add mobile var

--- src/comps/ui/HomeHero.tsx
+++ src/comps/ui/HomeHero.tsx
@@ -1,16 +1,21 @@
 import Button from "../primitive/Button";
+import Picture from "../primitive/Picture";
 
 export default function HomeHero() {
   return (
     <section className="relative">
       <div className="absolute z-10 block h-62.5 pointer-events-none bg-linear-to-b from-[#00000040] to-[#00000000] "></div>
-      <img className="brightness-80" src="./img/home-hero.webp" alt="" />
-      <div className="absolute z-20 bottom-0 w-full px-10 py-16">
-        <h1 className="font-philosopher text-[82px] text-white max-w-[9em]">
+      <Picture
+        mdSrc="./img/home-hero.webp"
+        smSrc="./img/home-hero-sm.png"
+        className="brightness-80"
+      />
+      <div className="absolute z-20 bottom-0 w-full px-2.5 md:px-10 py-6 md:py-16">
+        <h1 className="font-philosopher text-[40px] md:text-[82px] mb-4 md:mb-6 text-white mx-auto md:mx-0 max-w-[9em] md:max-w-[9em] text-center md:text-left">
           Curating the Future of Asset Ownership
         </h1>
-        <div className="flex items-center justify-between">
-          <p className="font-geist leading-[160%] max-w-[45em] text-white">
+        <div className="flex flex-col gap-6 md:flex-row items-center justify-between">
+          <p className="font-geist leading-[160%] max-w-[45em] text-white text-center md:text-left">
             A curated auction platform where real-world assets are tokenized as
             NFTs and traded with full transparency, enabling secure ownership,
             verified provenance, and global market access.
