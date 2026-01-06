04bd42e09757286a1482a7670e82bbb301a8bf2d
teguhirvan85-hue <teguhirvan85@gmail.com>
2026-01-06 14:56:38 +0700

    HomeAbout: add mobile var

--- src/comps/ui/HomeAboutUs.tsx
+++ src/comps/ui/HomeAboutUs.tsx
@@ -1,38 +1,39 @@
 import Link from "next/link";
 import SectionWrapper from "../primitive/SectionWrapper";
+import Picture from "../primitive/Picture";
 
 export default function HomeAboutUs() {
   return (
     <SectionWrapper className="md:bg-white md:px-10 md:py-30">
-      <p className="uppercase font-inter text-[18px] font-medium opacity-60 mb-4">
+      <p className="uppercase font-inter text-lg md:text-[18px] font-medium opacity-60 mb-4 text-center md:text-left">
         about us
       </p>
-      <div className="flex items-end justify-between mb-10">
-        <h2 className="tracking-[-2%] font-medium font-inter text-[40px] basis-1/2 leading-[120%] max-w-[19em]">
+      <div className="flex flex-col md:flex-row items-end justify-between mb-3 md:mb-10 gap-3">
+        <h2 className="tracking-[-2%] font-medium font-inter text-2xl md:text-[40px] basis-1/2 leading-[120%] max-w-[19em] text-center md:text-left">
           Curating <span className="text-[#6C737F]">Real-World Assets</span> To
           Shape The Future Of Ownership Through
           <span className="text-[#6C737F]">Transparent</span> And{" "}
           <span className="text-[#6C737F]">Trusted Auctions.</span>
         </h2>
-        <p className="text-[18px] basis-1/2 max-w-[35em]">
+        <p className="text-[13px] md:text-[18px] basis-1/2 max-w-[35em]  text-center md:text-left">
           We curate and auction real-world assets as NFTs, ensuring verified
           provenance, transparent auctions, and secure ownership worldwide.
         </p>
       </div>
       <div className="relative">
-        <div className="absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] bg-white w-50 h-50 z-10 grid items-center justify-center rounded-full shadow-[inset_0_0_0_17px_#f4f4f4] text-[20px]">
+        <div className="absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] bg-white w-33.25 md:w-50 h-33.25 md:h-50 z-10 grid items-center justify-center rounded-full shadow-[inset_0_0_0_12px_#f4f4f4] md:shadow-[inset_0_0_0_17px_#f4f4f4] text-[13px] md:text-[20px]">
           <Link href="#">About Us</Link>
         </div>
-        <div className="flex gap-7">
-          <img
-            className="max-w-[calc(50vw-3.5rem)]"
-            src="./img/home-about-1.png"
-            alt=""
+        <div className="flex gap-3 md:gap-7">
+          <Picture
+            smSrc="./img/home-about-1-sm.png"
+            mdSrc="./img/home-about-1.png"
+            className=" md:max-w-[calc(50vw-3.5rem)]"
           />
-          <img
-            className="max-w-[calc(50vw-3.5rem)]"
-            src="./img/home-about-2.png"
-            alt=""
+          <Picture
+            smSrc="./img/home-about-2-sm.png"
+            mdSrc="./img/home-about-2.png"
+            className=" md:max-w-[calc(50vw-3.5rem)]"
           />
         </div>
       </div>
