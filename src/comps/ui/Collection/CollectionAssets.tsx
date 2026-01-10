ead1717dd574e9a4b1833afa5017d1a26f7d3eee
teguhirvan85-hue <teguhirvan85@gmail.com>
2026-01-10 11:02:54 +0700

    CollectionAssets: init header & pagination

--- src/comps/ui/Collection/CollectionAssets.tsx
+++ src/comps/ui/Collection/CollectionAssets.tsx
@@ -1,5 +1,59 @@
 import Button from "@/comps/primitive/Button";
 import SectionWrapper from "@/comps/primitive/SectionWrapper";
+import Link from "next/link";
+import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+
+function DropdownButton({
+  children,
+  fontSize = "text-[18px]",
+}: {
+  children: ReactNode;
+  fontSize?: string;
+}) {
+  return (
+    <Button className="p-0 flex items-center gap-3 bg-transparent">
+      <span className={twMerge("capitalize font-medium", fontSize)}>
+        {children}
+      </span>
+      <img className="w-5" src="./img/chevron-down.png" alt="" />
+    </Button>
+  );
+}
+
+function Pagination() {
+  return (
+    <div className="flex items-center justify-center gap-12 ">
+      <Button className="p-2">
+        <img src="./img/chevron-left.png" className="w-6" alt="" />
+      </Button>
+      <nav className="h-full ">
+        <ul className="flex items-center">
+          {Array.from({ length: 5 }).map((e, i) => {
+            const ACTIVE_PAGE = 3;
+            const baseClass = "font-medium text-xl py-1.25 px-2 border-b";
+            return (
+              <li
+                key={i}
+                className={twMerge(
+                  baseClass,
+                  i + 1 == ACTIVE_PAGE
+                    ? "text-black border-b-2 border-b-[#6C737F]"
+                    : "text-[#6C737F] border-b-[#6C737F]"
+                )}
+              >
+                <Link href="#">{i + 1}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      <Button className="p-2">
+        <img src="./img/chevron-right.png" className="w-6" alt="" />
+      </Button>
+    </div>
+  );
+}
 
 export default function CollectionAssets() {
   return (
@@ -17,6 +71,24 @@ export default function CollectionAssets() {
           <img className="w-4.5" src="./img/search-dark.png" alt="" />
         </Button>
       </form>
+      <div className="flex items-center justify-between md:mt-10">
+        <div className="flex items-center gap-5 ">
+          <DropdownButton>category</DropdownButton>
+          <DropdownButton>era</DropdownButton>
+          <DropdownButton>edition/supply</DropdownButton>
+        </div>
+
+        <div className="flex items-center">
+          <p className="capitalize font-medium text-[18px]">
+            56 items sort by :
+          </p>
+          <DropdownButton fontSize="text-[18px]">
+            highest edition
+          </DropdownButton>
+        </div>
+      </div>
+
+      <Pagination />
     </SectionWrapper>
   );
 }
